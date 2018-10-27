c8960812e261a7628bb5300f360bdbd0324e98c8
Jorge Diaz <jorge7soccer@gmail.com>
2018-10-27 18:30:07 -0400

    Added comment with author names.

--- src/cpusim/CPUSIM.java
+++ src/cpusim/CPUSIM.java
@@ -1,9 +1,10 @@
 package cpusim;
-
+/*
+    Authors: Jorge Diaz & Enrique Gambra
+*/
 public class CPUSIM {
     public static void main(String[] args) {
-        GuiBuilder displayGUI = new GuiBuilder();
-        
+        GuiBuilder displayGUI = new GuiBuilder(); 
     }
     
 }
